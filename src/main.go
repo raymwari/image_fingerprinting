1423ddb4e900fd2a16508c94da7f0e407b0a3f60
raymwari <mwari.ray@gmail.com>
2025-07-25 01:16:20 -0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -15,9 +15,11 @@ import (
 var dct_mat []int16 // dct matrix
 var q_tabs []q_tab  // quantization tables (luminance | chrominance)
 
-func ham_d(hash, hash1 string) (int, int) {
+func ham_d(hash, 
+	   hash1 string) (int, 
+			  int) {
 	if len(hash) != len(hash1) {
-		log.Fatalln("encountered unequal hashes")
+		log.Fatalln("encountered unequal len hashes")
 	}
 	dist := 0
 	l := len(hash)
@@ -61,7 +63,7 @@ func up(arg string) string {
 	out := idct(dct_mat, // 3. inverse dct (luminance only)
 		blks_x,
 		blks_y)
-	return SHA512(string(out))
+	return SHA512(string(out)) // 4. hashing 
 }
 
 func main() {
@@ -69,20 +71,22 @@ func main() {
 		log.Fatalln("2 args expected... try again!")
 	}
 	args := os.Args
-	if _, err := os.Stat(args[1]); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat(args[1]); errors.Is(err, 
+						 os.ErrNotExist) {
 		log.Fatalln("file doesn't exist:",
 			args[1])
 	}
-	if _, err := os.Stat(args[2]); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat(args[2]); errors.Is(err, 
+						 os.ErrNotExist) {
 		log.Fatalln("file doesn't exist:",
 			args[2])
 	}
-	d, l := ham_d(up(args[2]),
+	d, l := ham_d(up(args[2]), // 5. hamming distance
 		up(args[1]))
 
 	total := l * 8
-	matching := float64(total) - float64(d)
-	perc := (matching / float64(total))
+	matching := float64(total) - float64(d) 
+	perc := (matching / float64(total)) // 6. similarity score
 	fmt.Printf("Images are %v percent similar ",
 		(perc * 100))
 }
